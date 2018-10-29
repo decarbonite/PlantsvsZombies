0203aa4d967fb3614c9c4b67f71b9e8a8c8f184b
Mrunal <mrunal.patel1997@gmail.com>
2018-10-29 12:34:43 -0400

    Fixed getter and setters for Player. Added Console input/output.

--- SouceCode/src/Player.java
+++ SouceCode/src/Player.java
@@ -7,13 +7,24 @@ import java.io.*;
 
 public class Player {
     private String playerName;
-    private int money;
+	private int money;
     private int score;
-    private static int totalRounds;
+    //private static int totalRounds;
 
     public Player () {
 		try {
-			TakeInputs();
+			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+			System.out.print("Enter Player Name\n");
+			playerName = bufferedReader.readLine();
+			setPlayerName(playerName);
+			setMoney(100);
+			System.out.println ("Player Name: " + getPlayerName()+ "/n Money: " + getMoney()+ "/n Score: " + getScore());
+			System.out.print("Enter Number of Rounds to Play\n");
+			try{
+				//totalRounds = Integer.parseInt(bufferedReader.readLine());
+			}catch(NumberFormatException nfe){
+				System.err.println("Invalid Format!");
+			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			System.out.println("Something went Wrong");
@@ -22,24 +33,27 @@ public class Player {
 		this.score = 0;
 	}
 
-	public void updateScore() {
-		//this.score =
+	public String getPlayerName() {
+		return playerName;
 	}
 
-	public void Money() {
-		
+	public void setPlayerName(String playerName) {
+		this.playerName = playerName;
 	}
 
-	public void TakeInputs() throws IOException{
-		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.print("Enter Player Name\n");
-        playerName = bufferedReader.readLine();
-        System.out.print("Enter Number of Rounds to Play\n");
-        try{
-            totalRounds = Integer.parseInt(bufferedReader.readLine());
-        }catch(NumberFormatException nfe){
-            System.err.println("Invalid Format!");
-        }
-        
+	public int getMoney() {
+		return money;
+	}
+
+	public void setMoney(int money) {
+		this.money = money;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public void updateScore(int score) {
+		this.score = score;
 	}
 }
