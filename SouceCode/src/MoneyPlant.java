069d81fc372b4c93f89a263537e3ec7d842c4538
Ahmed <ahmedromih@cmail.carleton.ca>
2018-10-29 07:22:54 -0400

    Added image of UML and few comments added

--- SouceCode/src/MoneyPlant.java
+++ SouceCode/src/MoneyPlant.java
@@ -2,8 +2,17 @@
  * This is a plant class that generates money
  * @author Dmytro Sytnik (VanArman)
  * @version 13 October, 2018
+ *
+ * @author Ahmed Romih (decarbonite)
+ * @version 26 October, 2018
  */
 public class MoneyPlant extends Plant {
+
+    /**
+     * Initializes a new MoneyPlant
+     * @param name Plant's name
+     * @param health Plant's health
+     */
     public MoneyPlant(String name, int health){
         super(name, health);
     }
